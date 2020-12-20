2e88ba72355fab285e2a9c98ad812e52996edc92
Hugo Roca <hugo.rock20@hotmail.com>
2020-12-19 23:39:47 -0500

    edit view user

--- app/views/user.php
+++ app/views/user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 ob_start();
 session_start();
 
@@ -6,6 +6,25 @@ if (!isset($_SESSION["name"])) {
   header("Location: login.html");
 } else {
   require 'head.php';
+
+  if ($_SESSION["access"]) {
+?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <strong>Users</strong> List
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php
+  }
 }
 
 require 'footer.php';
